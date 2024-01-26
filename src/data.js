5ad20ade1c8821b17ad8d925ea30cff96fc7dc7a
Razvan <razvan@whitecitycode.com>
2024-01-26 14:26:27 +0200

    feat:activities section

--- src/data.js
+++ src/data.js
@@ -7,6 +7,33 @@ export const navLinks = [
     route: "Festival",
     href: "#festival",
     classes:
-      " text-sm font-semibold drop-shadow text-accentHover hover:text-blue-700 transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+      "text-sm font-semibold drop-shadow text-accentHover hover:text-blue-700 transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+  },
+];
+
+export const activities = [
+  {
+    title: "CoderDojo",
+    description:
+      "CoderDojo Alba Iulia este parte a unei inițiative globale de a crea cluburi de programare pentru copii și adolescenți. Dojo-ul din Alba Iulia este o inițiativă a AI3, primul cluster de IT din județul Alba.",
+    image: "/coder_dojo.svg",
+    buttonDesc: "Website CoderDojo",
+    href: "https://coderdojo.ai3.ro",
+  },
+  {
+    title: "Ateliere",
+    description:
+      "Ne întâlnim în fiecare Joi de la ora 18:00. La Ateliere facem schimb de know-how tehnic, la centrul Conventum, în săptămânile impare. La Anti-Ateliere, în săptămânile pare, socializăm informal la  framm's.",
+    image: "/ateliere.avif",
+    buttonDesc: "Mai multe detalii",
+    href: "https://www.facebook.com/profile.php?id=61553933267429",
+  },
+  {
+    title: "Festival Digital",
+    description:
+      "Te invităm să explorezi primul Festival Digital din județul Alba, unde trecutul longeviv întâlnește manifestarea artistică digitală în cel mai important oraș istoric din România: Alba Iulia, Cealaltă Capitală.",
+    image: "/digital_art.avif",
+    buttonDesc: "Website Festival - cand o fi",
+    href: "#",
   },
 ];
