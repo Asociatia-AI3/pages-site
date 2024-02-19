fea6842949f3569e40346a9b3435481caff12366
Razvan <razvan@whitecitycode.com>
2024-02-19 14:42:40 +0200

    fix: change altnet website link

--- src/data.js
+++ src/data.js
@@ -91,45 +91,36 @@ export const partners = [
 export const projects = [
   {
     name: "altnet",
-    logo: "/projects/iflow_logo.webp",
     bg: "/projects/altnet.avif",
-    link: "https://getonline.ru",
+    link: "https://altnet.ro",
     developer: "WhiteCity Code",
-    developerLogo: "/projects/iflow_logo.webp",
     developerLink: "https://whitecitycode.com",
     description:
       "Descoperă alternative nelimitate și o viziune diferită pentru afacerea ta în online. De la site, design și conținut, până la marketing, brandul tău devine altceva. ",
   },
   {
     name: "Echipa De Digitalizare",
-    logo: "/projects/digitalizare.png",
     bg: "/projects/digitalizare.avif",
     link: "https://echipadedigitalizare.ro/",
     developer: "Echipa de Digitalizare",
-    developerLogo: "/projects/digitalizare.png",
     developerLink: "https://echipadedigitalizare.ro/",
-    height: "max-h-14",
     description:
       "Faci primii pași spre digitalizare, alături de o echipă de specialiști în soluții tehnologice dedicate IMM-urilor. ",
   },
   {
     name: "StockRx",
-    logo: "/projects/stockrx.png",
     bg: "/projects/stockrx.avif",
     link: "https://stockrx.uk/",
     developer: "StockRx",
-    developerLogo: "/projects/stockrx_logo.png",
     developerLink: "https://www.stockrx.uk/",
     description:
       "Activezi în industria farmaceutică? Gestionează mai ușor și ține evidența stocurilor cu un software de management intuitiv, creat după nevoile tale.",
   },
   {
     name: "WhiteRCPT",
-    logo: "/projects/whitercpt.svg",
     bg: "/projects/whitercpt.avif",
     link: "https://whitercpt.ro/",
     developer: "WhiteCity Code",
-    developerLogo: "/projects/whitecitycode.svg",
     developerLink: "https://www.whitecitycode.com/",
     description:
       "Scanează, administrează și sortează cu ajutorul inteligenței artificiale bonurile cumpărăturilor tale, în timp ce îți eficientizezi bugetul, totul cu o singură aplicație.",
